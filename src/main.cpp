cc3e51b76e55182dfb4f07894b1ce89bc66c1ec7
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-03-08 22:02:39 +0100

    Added cxx standard version to cmakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,10 @@ int main(int argc, char * argv[]) {
   int x = 6;
   std::cout << "X: " << x << ", Address is: " << &x << std::endl;
   std::cout << "Argument Count is: " << argc << std::endl;
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Learning_cmake_VERSION_MAJOR << "." << Learning_cmake_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+
+  // report version
+  std::cout << argv[0] << " Version " << Learning_cmake_VERSION_MAJOR << "." << Learning_cmake_VERSION_MINOR << std::endl;
+  std::cout << "Usage: " << argv[0] << " number" << std::endl;
+  
   return 0;
 }
